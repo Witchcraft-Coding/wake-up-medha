988a2eaa620b51079936cbaf2ead9972a4c826fa
Param Siddharth <contact@paramsid.com>
2024-10-16 23:40:58 +0530

    Add colour picker, fix formatting
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import { Hue, Saturation, useColor } from "react-color-palette";
+import invert from "invert-color";
+
+import "react-color-palette/css";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [colour, setColour] = useColor("white");
+	const invertedColour = useMemo(() => invert(colour.hex, true), [colour]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div
+				style={{
+					height: "100vh",
+					width: "100vw",
+					backgroundColor: colour.hex,
+					color: invertedColour,
+					display: "flex",
+					flexDirection: "column",
+					gap: "0",
+					alignItems: "center",
+					paddingTop: "30px",
+				}}
+			>
+				<div
+					style={{
+						width: "min(200px, 95vw)",
+						height: "200px",
+						display: "flex",
+						flexDirection: "column",
+						gap: "0",
+					}}
+				>
+					<Saturation
+						height={200}
+						color={colour}
+						onChange={setColour}
+					/>
+					<div
+						style={{
+							padding: "3px",
+							backgroundColor: colour.hex,
+						}}
+					>
+						<Hue color={colour} onChange={setColour} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
